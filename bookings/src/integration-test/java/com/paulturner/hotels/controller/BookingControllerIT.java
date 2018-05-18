7a46da10efc4ce601951941482a6661394ff2aa3
pauljeremyturner <pauljeremyturner@yahoo.com>
2018-05-18 11:32:35 +0200

    final changes  * fixed docker compose rule integration test  * updated dependencies

--- bookings/src/integration-test/java/com/paulturner/hotels/controller/BookingControllerIT.java
+++ bookings/src/integration-test/java/com/paulturner/hotels/controller/BookingControllerIT.java
@@ -6,14 +6,20 @@ import com.palantir.docker.compose.connection.waiting.HealthChecks;
 import com.paulturner.hotels.model.*;
 import org.awaitility.Awaitility;
 import org.awaitility.Duration;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
+
+import static org.junit.Assert.assertThat;
 
 public class BookingControllerIT {
 
@@ -28,7 +34,7 @@ public class BookingControllerIT {
             .build();
 
     @Test
-    public void shouldStoreAndReadBooking() {
+    public void shouldStoreAndReadBooking() throws Exception {
 
         DockerPort bookings = docker.containers()
                 .container("bookings")
@@ -51,22 +57,32 @@ public class BookingControllerIT {
                 request.getCustomer(),
                 new Hotel("3", "Ibis Glasgow City Centre")
         );
+
         HttpEntity<BookingRequest> bookingRequestHttpEntity = new HttpEntity<>(request);
-        restTemplate.exchange(url, HttpMethod.POST, bookingRequestHttpEntity, Booking.class);
 
-        Awaitility
-                .await().
-                atMost(Duration.TEN_SECONDS)
-                .until(() -> assertResponse(url + request.getId(), expectedBooking));
+        Awaitility.await().atMost(Duration.TEN_SECONDS).until( () ->
+                {
+                    try {
+                        restTemplate.exchange(url, HttpMethod.POST, bookingRequestHttpEntity, Booking.class);
+                        return true;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                }
+        );
+
+        Awaitility.await().atMost(Duration.TEN_SECONDS).until( () ->
+                {
+                    try {
+                        final ResponseEntity<Booking> responseEntity = restTemplate.exchange(url + request.getId(), HttpMethod.GET, null, Booking.class);
+                        final Booking actualBooking = responseEntity.getBody();
+                        return actualBooking.getId().equals(expectedBooking.getId());
+                    } catch (Exception e) {
+                        return false;
+                    }
+                }
+        );
     }
 
-    private boolean assertResponse(String url, Booking expectedBooking) {
-        final ResponseEntity<Booking> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, Booking.class);
-
-        final Booking actualBooking = responseEntity.getBody();
-
-        boolean bookingIdsOk = actualBooking.getId().equals(expectedBooking.getId());
-        return bookingIdsOk;
-    }
 
 }
